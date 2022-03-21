88ac7dd3f832e54a3449974f2c00194425141357
Chidera Okeke <okekechidera97@gmail.com>
2022-03-21 14:36:25 +0100

    working on http request fxn

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.getDoc = functions.https.onRequest((request, response) => {
+  admin.database().ref("/tasks").get().then((snapshot) => {
+    response.send(snapshot.val());
+  }).catch((error) => {
+    console.log(error.message);
+  });
+});
