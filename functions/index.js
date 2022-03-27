ba1fe422186a0ec8db844aa6b014b6deff280b7f
Chidera Okeke <okekechidera97@gmail.com>
2022-03-27 22:42:09 +0100

    callable function

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,26 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// http callable function
+exports.helloFromEducative = functions.https.onCall((data, context) => {
+  const name = context.auth.token.name || "user";
+  // const picture = context.auth.token.picture || null;
+  // const email = context.auth.token.email || null;
 
-exports.getDoc = functions.https.onRequest((request, response) => {
-  admin.database().ref("/tasks").get().then((snapshot) => {
-    response.send(snapshot.val());
-  }).catch((error) => {
-    console.log(error.message);
-  });
+  return `Hello ${name} from Educative`;
+});
+
+// http request function
+exports.getTasks = functions.https.onRequest((request, response) => {
+  admin
+    .database()
+    .ref("/tasks")
+    .get()
+    .then((snapshot) => {
+      response.send(snapshot.val());
+    })
+    .catch((error) => {
+      console.log(error.message);
+      response.status(500).send(error.message);
+    });
 });
